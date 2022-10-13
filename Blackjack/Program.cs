f4af894c2a955ef7b0854f70c832df6501534c40
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2022-10-13 13:25:43 +0200

    .

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -14,10 +14,12 @@ namespace Blackjack
                 //Menu goes here!
                 Console.WriteLine("Menu");
                 choice = Console.ReadKey().Key;
+
                 switch (choice)
                 {
                     case ConsoleKey.D1: // Start game
-                        Game();
+                        lastWinner = GameSystem.Game();
+                        Console.WriteLine(lastWinner + " vann.");
                         break;
                     case ConsoleKey.D2: // Show previous winner
                         break;
@@ -29,83 +31,5 @@ namespace Blackjack
                 }
             }
         }
-        static String Game()
-        {
-            List<int> player = new List<int>();
-            List<int> bank = new List<int>();
-            for(int i = 0; i < 2; i++)
-            {
-                player.Add(DealCard());
-                bank.Add(DealCard());
-            }
-            bool draKort = true;
-            while (draKort)
-            {
-                Console.WriteLine("Dra Kort");
-                if (Bust(player))
-                {
-                    Console.WriteLine("Busted");
-                    return "Banken vann!";
-                }else if (TwentyOne(player))
-                {
-                    Console.WriteLine("victory");
-                    return "Spelaren vann!";
-                }
-                else
-                {
-                    bool inputCheck = true;
-                    while (inputCheck)
-                    {
-                        Console.WriteLine("Dra ett till kort? j/n");
-                        switch (Console.ReadKey().Key)
-                        {
-                            case ConsoleKey.J:
-                                player.Add(DealCard());
-                                inputCheck = false;
-                                break;
-                            case ConsoleKey.N:
-                                inputCheck = false;
-                                draKort = false;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("End");
-
-            return "player";
-        }
-        static int DealCard(int i = 10)
-        {
-            Random range = new Random();
-            int card = range.Next(i + 1);
-            return card;
-        }
-        static bool Bust(List<int> player)
-        {
-            if (player.Sum() <= 21)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        static bool TwentyOne(List<int> player)
-        {
-            if (player.Sum() == 21)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-
     }
 }
